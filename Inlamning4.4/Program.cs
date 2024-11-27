ae2fa66fa9d1ff1ae25f40d3c0a65adecb0e5a37
leit07001 <leit07001@E-PW05C37S>
2024-11-27 12:09:29 +0100

    Add project files.

---
+++ Inlamning4.4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ange låtens längd i minuter: ");
+        int minuter = int.Parse(Console.ReadLine());
+
+        Console.Write("Ange låtens längd i sekunder: ");
+        int sekunder = int.Parse(Console.ReadLine());
+
+  
+        int totalSekunder = minuter * 60 + sekunder;
+
+ 
+        int minLängd = 2 * 60 + 45;  
+        int maxLängd = 4 * 60 + 20; 
+
+      
+        if (totalSekunder >= minLängd && totalSekunder <= maxLängd)
+        {
+            Console.WriteLine("Låten får spelas på radiostationen.");
+        }
+        else
+        {
+            Console.WriteLine("Låten får inte spelas på radiostationen.");
+        }
+    }
+}
